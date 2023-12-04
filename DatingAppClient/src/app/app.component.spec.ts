aa5dca462bc074b476f3aeba25ceffe3eaa2154f
Igor Santiago <contato.igorsantiago@outlook.com>
2023-12-04 06:52:04 -0300

    Rename client project (angular) to remove especial characters.

--- DatingAppClient/src/app/app.component.spec.ts
+++ DatingAppClient/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DatingApp.Client'`, () => {
+  it(`should have as title 'DatingAppClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DatingApp.Client');
+    expect(app.title).toEqual('DatingAppClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DatingApp.Client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DatingAppClient app is running!');
   });
 });
